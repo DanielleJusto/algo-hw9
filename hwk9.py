74b680cd82ce8e57d4d5c72e7e442b807c65cbd0
DanielleJusto <danielleejusto@gmail.com>
2025-11-17 18:41:03 -0500

    Add docstrings for existing functions

--- hwk9.py
+++ hwk9.py
@@ -1,16 +1,28 @@
 # Name:       - Danielle Justo
 # Peers:      - Isabelle Wang
 # References: - https://www.geeksforgeeks.org/python/python-convert-list-characters-string/
-#              https://www.geeksforgeeks.org/r-language/create-a-choropleth-map-by-using-plotly-package-in-r/
-#              https://statisticsglobe.com/state-name-abbreviation-r
-#              https://plotly.com/python/map-configuration/
+#               https://www.geeksforgeeks.org/r-language/create-a-choropleth-map-by-using-plotly-package-in-r/
+#               https://statisticsglobe.com/state-name-abbreviation-r
+#               https://plotly.com/python/map-configuration/
 
 import csv  
 import pandas   
 
 def greedy1(map:dict) -> dict:
     """ Generates a color mapping given a map/graph in the form of a dictionary
-    
+    :param map: (dict) A map or in the form of a dictionary
+    :return: (dict) A dictionary mapping map keys to colors
+    >>> print(greedy1(map))
+    {'Alabama': 1, 'Alaska': 1, 'Arizona': 1, 'Arkansas': 1, 'California': 2, 
+    'Colorado': 2, 'Connecticut': 1, 'Delaware': 1, 'Florida': 2, 'Georgia': 3, 
+    'Hawaii': 1, 'Idaho': 1, 'Illinois': 1, 'Indiana': 2, 'Iowa': 2, 'Kansas': 1, 
+    'Kentucky': 3, 'Louisiana': 2, 'Maine': 1, 'Maryland': 2, 'Massachusetts': 2, 
+    'Michigan': 3, 'Minnesota': 1, 'Mississippi': 3, 'Missouri': 4, 'Montana': 2, 
+    'Nebraska': 3, 'Nevada': 3, 'New_Hampshire': 3, 'New_Jersey': 2, 'New_Mexico': 3, 
+    'New_York': 3, 'North_Carolina': 1, 'North_Dakota': 3, 'Ohio': 1, 'Oklahoma': 5, 
+    'Oregon': 4, 'Pennsylvania': 4, 'Rhode_Island': 4, 'South_Carolina': 2, 'South_Dakota': 4, 
+    'Tennessee': 2, 'Texas': 4, 'Utah': 4, 'Vermont': 1, 'Virginia': 4, 'Washington': 2, 
+    'West_Virginia': 5, 'Wisconsin': 4, 'Wyoming': 5}
     """
     colors_used = {1}  # Track the colors used
     current_color = 1  # Track the most recent color added
@@ -43,6 +55,11 @@ def greedy2(map:dict) -> dict:
 
 def as_list(neighbors: str) -> list :
     """ Parse neighbors string into a list of strings
+    :param neighbors: (str) A list of neighboring states in the form of a string of chars
+    :return: (list) The list of neighboring states parsed into a list of strings
+    >>> list_as_str = 'Florida, Georgia, Mississippi, Tennessee'
+    >>> print(as_list(list_as_str))
+    ['Florida', 'Georgia', 'Mississippi', 'Tennessee']
     """
     # Create an array to store neighbors
     slots = 1
@@ -63,6 +80,15 @@ def as_list(neighbors: str) -> list :
     neighbors_arr[index] = word
     return neighbors_arr
 
+def testHelper():
+        filename = 'color-US-states.csv'
+        with open(filename) as csv_file:    
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            header = csv_reader.__next__()
+            row = csv_reader.__next__()
+        print(row[1])
+        print(as_list(row[1]))
+
 def main():
     map = dict()
     filename = 'color-US-states.csv'
@@ -79,15 +105,16 @@ def main():
                             'Num_n' : int(row[2])
                             }    
         print("csv successfully converted!")
-        print("Pick a greedy map coloring algorithm.\n(0) To quit the program\n(1) For greedy algorithm 1\n(2) For greedy algorithm 2")
         end_program = False
         while(end_program == False):
             try:
+                print("Pick a greedy map coloring algorithm.\n(0) To quit the program\n(1) For greedy algorithm 1\n(2) For greedy algorithm 2")
                 choice = int(input())
                 if choice == 0:
                     end_program = True
                 if choice == 1:
                     coloring1 = greedy1(map)
+                    print(coloring1)
                     try:
                         with open("greedy1.csv", 'w') as file:
                             writer = csv.writer(file)
@@ -103,8 +130,8 @@ def main():
             except ValueError:
                 print("Pick a greedy map coloring algorithm.\n(0) To quit the program\n(1) For greedy algorithm 1\n(2) For greedy algorithm 2")
 
-            
     except FileNotFoundError:
         print(f"Error opening {filename}")
 
 main()
+# testHelper()
