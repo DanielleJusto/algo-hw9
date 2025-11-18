20022cb1a4d2de02c50477d783e29b6f8f08f516
DanielleJusto <danielleejusto@gmail.com>
2025-11-17 22:13:42 -0500

    Izzy docstrings

--- Mapcoloring.R
+++ Mapcoloring.R
@@ -3,6 +3,7 @@ library(sf)
 library(leaflet)
 library(maps)
 library(tmaptools)
+library(plotly)
 
 # Read in Data
 greedy1 <- read_csv("greedy1.csv")
@@ -23,8 +24,36 @@ map <- plot_geo(
   locations = ~abbrev,
   z = ~Color,
   locationmode = "USA-states"
-) |> layout(title = "Map Coloring: Greedy Algorithm 1", 
-            geo = list(scope = "usa"))
+  ) |> layout(title = "Map Coloring: Greedy Algorithm 1", 
+          geo = list(scope = "usa")) 
 # Display Map
 map
+
+# Read in Data
+greedy2 <- read_csv("greedy2.csv")
+# Format to Long
+greedy2 <- greedy2 |>
+  pivot_longer(
+    cols =  everything(),
+    names_to = "State",
+    values_to = "Color"
+  )
+
+greedy2 <- greedy2[order(greedy2$State),]
+# Add State abbreviations for plotly
+greedy2 <- greedy2 |> mutate(abbrev = state.abb)
+
+
+# Create Map
+map2 <- plot_geo(
+  greedy2,
+  type = "choropleth",
+  locations = ~abbrev,
+  z = ~Color,
+  locationmode = "USA-states"
+) |> layout(title = "Map Coloring: Greedy Algorithm 2", 
+            geo = list(scope = "usa"))
+# Display Map
+map2
+
 
--- hwk9.py
+++ hwk9.py
@@ -6,7 +6,8 @@
 #               https://plotly.com/python/map-configuration/
 
 import csv  
-import pandas   
+import math
+from collections import deque
 
 def greedy1(map:dict) -> dict:
     """ Generates a color mapping given a map/graph in the form of a dictionary
@@ -27,7 +28,6 @@ def greedy1(map:dict) -> dict:
     colors_used = {1}  # Track the colors used
     current_color = 1  # Track the most recent color added
     color_mapping = {} # Dictionary of mapping to return
-    num_ops = 0        # Keep track of operations for analysis
 
     # Iterate through states
     for key in map.keys():
@@ -49,9 +49,61 @@ def greedy1(map:dict) -> dict:
         else:
             color_mapping[key] = list(available_colors)[0]
     return color_mapping
-
+            
 def greedy2(map:dict) -> dict:
-    return {}
+    """ Generates a color mapping given a map/graph in the form of a dictionary
+    :param map: (dict) A map or in the form of a dictionary
+    :return: (dict) A dictionary mapping map keys to colors
+    >>> print(greedy1(map))
+    {'Alabama': 1, 'Alaska': 1, 'Arizona': 1, 'Arkansas': 1, 'California': 2,
+    'Colorado': 2, 'Connecticut': 1, 'Delaware': 1, 'Florida': 2, 'Georgia': 3,
+    'Hawaii': 1, 'Idaho': 1, 'Illinois': 1, 'Indiana': 2, 'Iowa': 2, 'Kansas': 1,
+    'Kentucky': 3, 'Louisiana': 2, 'Maine': 1, 'Maryland': 2, 'Massachusetts': 2,
+    'Michigan': 3, 'Minnesota': 1, 'Mississippi': 3, 'Missouri': 4, 'Montana': 2,
+    'Nebraska': 3, 'Nevada': 3, 'New_Hampshire': 3, 'New_Jersey': 2, 'New_Mexico': 3,
+    'New_York': 3, 'North_Carolina': 1, 'North_Dakota': 3, 'Ohio': 1, 'Oklahoma': 5,
+    'Oregon': 4, 'Pennsylvania': 4, 'Rhode_Island': 4, 'South_Carolina': 2, 'South_Dakota': 4,
+    'Tennessee': 2, 'Texas': 4, 'Utah': 4, 'Vermont': 1, 'Virginia': 4, 'Washington': 2,
+    'West_Virginia': 5, 'Wisconsin': 4, 'Wyoming': 5}
+    """
+    search = deque()
+    visited = deque()
+    search += [list(map.keys())[0]] # Adds the first state to the queue 
+    color_mapping = {}
+    colors_used = {1}
+    current_color = 1
+    
+    while search: 
+        state = search.popleft()
+        if not state in visited:
+            visited += [state]
+            neighbors = map[state]['Neighbors']
+            # Put neighbor colors into a set
+            neighbor_colors = set()
+            # Iterate through neighbors
+            for n in neighbors:
+                # If the neighbors are colored (it's in color mapping), Add the color to the set
+                if n in color_mapping.keys():
+                    neighbor_colors.add(color_mapping[n])
+            # Take set difference of neighbor colors and colors used
+            available_colors = colors_used - neighbor_colors
+            # If colors_used == neighbor_colors, Add a new color
+            if available_colors == set():
+                current_color += 1
+                colors_used.add(current_color)
+                color_mapping[state] = current_color
+            # Else, use the first item in available colors
+            else:
+                color_mapping[state] = list(available_colors)[0]
+            # Add all neighbors to search queue
+            search += neighbors
+
+    # Color disconnected States
+    for state in list(map.keys()): 
+        if state not in color_mapping: 
+            color_mapping[state] = list(colors_used)[0]
+        
+    return color_mapping
 
 def as_list(neighbors: str) -> list :
     """ Parse neighbors string into a list of strings
@@ -79,16 +131,7 @@ def as_list(neighbors: str) -> list :
             word = ''
     neighbors_arr[index] = word
     return neighbors_arr
-
-def testHelper():
-        filename = 'color-US-states.csv'
-        with open(filename) as csv_file:    
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            header = csv_reader.__next__()
-            row = csv_reader.__next__()
-        print(row[1])
-        print(as_list(row[1]))
-
+    
 def main():
     map = dict()
     filename = 'color-US-states.csv'
@@ -106,6 +149,8 @@ def main():
                             }    
         print("csv successfully converted!")
         end_program = False
+
+
         while(end_program == False):
             try:
                 print("Pick a greedy map coloring algorithm.\n(0) To quit the program\n(1) For greedy algorithm 1\n(2) For greedy algorithm 2")
@@ -124,7 +169,16 @@ def main():
                     except FileNotFoundError:
                         print(f"Error writing to {file}")
                 if choice == 2:
-                    print(greedy2(map))
+                    coloring2 = greedy2(map)
+                    print(coloring2)
+                    try:
+                        with open("greedy2.csv", 'w') as file:
+                            writer = csv.writer(file)
+                            writer.writerow(coloring2.keys())
+                            writer.writerow(coloring2.values())
+                        print("Mapping saved to greedy2.csv.")
+                    except FileNotFoundError:
+                        print(f"Error writing to {file}")
                 elif not choice in [0,1,2]:
                     print("Pick a greedy map coloring algorithm.\n(0) To quit the program\n(1) For greedy algorithm 1\n(2) For greedy algorithm 2")
             except ValueError:
@@ -134,4 +188,3 @@ def main():
         print(f"Error opening {filename}")
 
 main()
-# testHelper()
